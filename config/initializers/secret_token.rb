f063168045de9413bf146ae2357814fc1029f9b4
Ben Zhang <bzhang@groupon.com>
2013-11-09 13:03:11 -0800

    empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vollot::Application.config.secret_key_base = 'e5e3baaf892e55899541ec1bb100657646e85f68f865d844b4bbcb932d70311a06a2fd146e4f5cdfe579c2fb50f51361fa15dfce163fb73b58cc4039c5e7c7e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vollot::Application.config.session_store :cookie_store, key: '_vollot_session'
