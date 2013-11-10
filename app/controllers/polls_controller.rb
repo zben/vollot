a675ce7dfaf8e67253f0bfc2d008cc56affc01e6
Ben Zhang <bzhang@groupon.com>
2013-11-10 15:44:50 -0800

    fixes

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -68,7 +68,7 @@ class PollsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_poll
-      @poll = Poll.find_by(permalink: params[:id])
+      @poll = Poll.find_by(permalink: params[:id].upcase)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
