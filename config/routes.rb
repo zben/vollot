7ed897f63ca3300196a71db4f84b46318916b216
Ben Zhang <bzhang@groupon.com>
2013-11-10 15:09:10 -0800

    add about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Vollot::Application.routes.draw do
+  get "pages/about"
   resources :polls, only: [:index, :new, :show, :create]
 
   get "vote/:choice_id" => "votes#create", as: "vote"
+  get "about" => "pages#about"
   get ":id" => "polls#show", as: "permalink"
   root "polls#new"
 end
